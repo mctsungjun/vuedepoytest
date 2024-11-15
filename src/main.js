7651444c7fbbd062b5bf9ccf2d5d39e226db427e
sung <m9716@naver.com>
2024-11-15 15:39:41 +0900

    3

--- src/main.js
+++ src/main.js
@@ -9,11 +9,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
-// import { useKakao } from 'vue3-kakao-maps/@utils';
+
 const app =createApp(App);
 app.use(router);
 app.mixin(mixins);
 app.use(store);
 app.use(VueSweetalert2);
-// app.use(useKakao);
+
 app.mount('#app');
