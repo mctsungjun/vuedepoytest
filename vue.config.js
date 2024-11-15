262efe5cdacf030c179820eb97397f977d705620
sung <m9716@naver.com>
2024-11-15 17:12:57 +0900

    22

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const target = "https://port-0-nodeservertest-m3cudz5w505940d1.sel4.cloudtype.app";
+const target = "https://nodeservertest:3000";
 
 //로컬
 // const target = "http://localhost:3000";
