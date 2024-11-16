669d6566df5847611206a39bd9fd039a0bc9f21a
sung <m9716@naver.com>
2024-11-16 18:39:14 +0900

    63

--- src/views/imageInsert.vue
+++ src/views/imageInsert.vue
@@ -21,7 +21,7 @@
             <div class="col-lg-3 col-md-4 col-sm-2" :key="i" v-for="(m,i) in productImage.filter(c=>c.p_type===1)">
               <div class="position-relative">
                 <img :src="`https://port-0-servernode-m3cudz5w505940d1.sel4.cloudtype.app/download/${productId}/${m.path}`" class="img-fluid" />
-                <div class="position-absolute top-0 end-0" style="cursor:pointer;" @click="deleteImage(m.id,m.path)">X</div>
+                <div class="position-absolute top-0 end-0" style="cursor:pointer;" @click="deleteImage(productId,m.path)">X</div>
               </div>
             </div>
           </div>
@@ -42,7 +42,7 @@
             <div class="col-lg-3 col-md-4 col-sm-2" :key="i" v-for="(m,i) in productImage.filter(c=>c.p_type==2)">
               <div class="position-relative">
                 <img :src="`https://port-0-servernode-m3cudz5w505940d1.sel4.cloudtype.app/download/${productId}/${m.path}`" class="img-fluid" />
-                <div class="position-absolute top-0 end-0" style="cursor:pointer;" @click="deleteImage(m.id,m.path)">X</div>
+                <div class="position-absolute top-0 end-0" style="cursor:pointer;" @click="deleteImage(productId,m.path)">X</div>
               </div>
             </div>
           </div>
@@ -73,7 +73,7 @@
             <div class="col-lg-6 col-md-4" :key="i" v-for="(m,i) in productImage.filter(c=>c.p_type==3)">
               <div class="position-relative">
                 <img :src="`https://port-0-servernode-m3cudz5w505940d1.sel4.cloudtype.app/download/${productId}/${m.path}`" class="img-fluid" />
-                <div class="position-absolute top-0 end-0" style="cursor:pointer;color:white;" @click="deleteImage(m.id,m.path)">X</div>
+                <div class="position-absolute top-0 end-0" style="cursor:pointer;color:white;" @click="deleteImage(productId,m.path)">X</div>
               </div>
             </div>
           </div>
